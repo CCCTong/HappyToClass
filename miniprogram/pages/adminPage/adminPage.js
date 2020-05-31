b849c469ecb932d4e1732a3133e05a2c6a45b416
bnothing <2268340097@qq.com>
2020-05-31 22:00:01 +0800

    创建了三个界面，学生主页、教师主页、管理员主页

---
+++ miniprogram/pages/adminPage/adminPage.js
@@ -0,0 +1,75 @@
+// miniprogram/pages/adminPage/adminPage.js
+// 管理员页面
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+    /**
+   * 管理账号，这算作教务功能
+   */
+  manageID: function() {
+    wx.navigateTo({
+      url: '../manageID/manageID',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
